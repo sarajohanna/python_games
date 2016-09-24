f7b8cef105e2a703a0caaa121ce6a4704bf6dd64
sarajohanna <sara.johanna.lindstrom@gmail.com>
2016-09-24 15:18:22 +0200

    Now a fully functional hangman game!
    The game now works and also added an ascii print out of the hangman himself!

--- hangman.py
+++ hangman.py
@@ -1,37 +1,82 @@
+import getpass
+
+# Define what should be printed out, hangman image
+#   +---+
+#   |   |
+#   |   O
+#   |  /|\
+#   |  / \
+#   |
+# =====
+
+h10 = "  +---+\n  |   |\n  |   O\n  |  /|\ \n  |  / \ \n  |\n====="
+h9 = "  +---+\n  |   |\n  |   O\n  |  /|\ \n  |  / \n  |\n====="
+h8 = "  +---+\n  |   |\n  |   O\n  |  /|\ \n  |\n  |\n====="
+h7 = "  +---+\n  |   |\n  |   O\n  |  /|\n  |\n  |\n====="
+h6 = "  +---+\n  |   |\n  |   O\n  |   |\n  |\n  |\n====="
+h5 = "  +---+\n  |   |\n  |   O\n  |\n  |\n  |\n====="
+h4 = "  +---+\n  |   |\n  |\n  |\n  |\n  |\n====="
+h3 = "  +---+\n  |\n  |\n  |\n  |\n  |\n====="
+h2 = "  |\n  |\n  |\n  |\n  |\n====="
+h1 = "====="
+
+# Put the cases in a list and later use the list to print out each case 
+# when a faulty guess occurs
+image_list = [h10, h9, h8, h7, h6, h5, h4, h3, h2, h1]
+
+# Give some instructions
+print "\n"
+print "Welcome to this awesome game of hangman!"
+print "One of you will type a word, and the other one will guess the letters."
+print "You can make {} mistakes.".format(len(image_list))
+print "\n"
+
 
 # Define the two players
 players = [" ", " "]
 players[0] = raw_input("Who wants to give me a word? ")
 players[1] = raw_input("Nice, so who is going to do the guessing? ")
+print "\n"
 
-# Ask player 1 for a word and create a list with the characters in the word
-word = list(raw_input("{}, give me a word, capital letters please! ".format(players[0])))
-
-print word
+# Ask player 1 for a word, create a list with the characters in the word.
+word = list(getpass.getpass("{}, give me a word!\nWhat you type won't show on the screen, let's keep it secret! ".format(players[0])).upper())
+print "\n"
 
 # Write out the empty spaces as a list, this needs to be a new list
 empty_word = ["_"] * len(word)
-print empty_word
+print empty_word, "\n"
 
+# Create a list to fill with the faulty guesses
 error_list = []
-count = 0
 
-while count < 12:
-    guess = raw_input("Give me a guess, {} ".format(players[1]))
+# Contiue the loop untill player have guessed wrong 11 times
+count = len(image_list)
+while count > 0:
     # Ask player 2 for a character and check if it is in the word.
-    # If it is, add it to the empty list, if not, add to the error list.
+    # If it is, add it to the empty list
+    guess = raw_input("Give me a guess {}! ".format(players[1])).upper()
+    print "\n"
     for pos, char in enumerate(word):
         if char == guess:
             empty_word[pos] = guess
-        else:
-            error_list.append(guess)
 
-    # count += 1
-    print empty_word
-    print error_list
+    # If the character is not in the word, append it to error list and count +1
+    if not (guess in word):
+        error_list.append(guess)
+        print image_list[(count - 1)]
+        count -= 1
 
-print word
+    # Print the count, the "empty" word and the error list
+    print "Guesses left: {}".format(count)
+    print "Word: {}".format(empty_word)
+    print "Used letters: {}".format(error_list)
+    print "\n\n\n"
+    # If the "empty" word equals the real word, the player has won!
+    if word == empty_word:
+        print "You won the game {}!!".format(players[1])
+        break
 
-# character in the "empty" list. If it is not, put it in a
-# "error list". If player two guesses wrong 11 times, he/she
-# loses.
+    # If the player has guessed wrong 11 times, the player loses
+    if count == 0:
+        print "You've used all your guessess. Better luck next time {}!".format(players[1])
+
